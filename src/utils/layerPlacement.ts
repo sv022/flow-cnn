eb9b337dad9ed6658b3f2480f48b243c2daa821f
sv022 <ssvatkin04@gmail.com>
2026-04-04 11:03:19 +0300

    chore: delete obsolete layerPlacement.ts

--- src/utils/layerPlacement.ts
+++
@@ -1,8 +0,0 @@
-import type { Layer } from "@/types";
-
-export function getLayerShiftX(layer: Layer): number {
-  if (layer.params.type === "conv" || layer.params.type === "pool") {
-    return 125;
-  }
-  return 200 + 125;
-}
