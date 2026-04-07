8675c8a0a43aa35bb2ede7c1cd466393a7c1ebcc
sv022 <ssvatkin04@gmail.com>
2026-04-07 18:34:28 +0300

    chore: remove unused button from sidebar

--- src/components/app/panel/Menu.vue
+++ src/components/app/panel/Menu.vue
@@ -1,11 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { cn } from "@/lib/utils";
-
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { Button } from "@/components/ui/button";
-import { LucideLogOut } from "lucide-vue-next";
 import ModelMenu from "./model/ModelMenu.vue";
 
 defineProps<{
@@ -18,21 +13,7 @@ defineProps<{
     <div class="mt-4 h-full w-full">
       <div class="flex w-full flex-col min-h-[calc(100vh-48px-36px-16px-32px)] lg:min-h-[calc(100vh-16px-40px-32px)] items-start space-y-1 px-2">
         <ModelMenu />
-        <div class="w-full grow flex items-end">
-          <TooltipProvider disable-hoverable-content>
-            <Tooltip :delay-duration="100">
-              <TooltipTrigger as-child>
-                <Button variant="outline" class="w-full justify-center h-10 mt-5">
-                  <span :class="cn('flex justify-center items-center', isOpen === false ? '' : 'mr-4')">
-                    <LucideLogOut />
-                  </span>
-                  <p :class="cn('whitespace-nowrap', !isOpen ? 'opacity-0 hidden' : 'opacity-100')">Sign out</p>
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent v-if="!isOpen" side="right"> Sign out </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </div>
+        <div class="w-full grow flex items-end"></div>
       </div>
     </div>
   </ScrollArea>
